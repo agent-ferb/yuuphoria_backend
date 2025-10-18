b892e3ba48db808374461f18d2364ce5df634c66
agent-ferb <shimerryyuu88@gmail.com>
2025-10-18 18:51:50 +0800

    Initial commit from Spring Initializr

---
+++ src/main/java/Yuu/yuuphoria/YuuphoriaApplication.java
@@ -0,0 +1,13 @@
+package Yuu.yuuphoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuuphoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuuphoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Yuu/yuuphoria/YuuphoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package Yuu.yuuphoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuuphoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
