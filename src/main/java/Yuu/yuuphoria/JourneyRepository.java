a161aa36c087f2bef25ce5e1a3776158df02d8f1
agent-ferb <shimerryyuu88@gmail.com>
2025-10-31 15:45:57 +0800

    feat: add journey repository

--- src/main/java/Yuu/yuuphoria/JourneyRepository.java
+++ src/main/java/Yuu/yuuphoria/JourneyRepository.java
@@ -1,4 +1,10 @@
 package Yuu.yuuphoria;
 
-public class JourneyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JourneyRepository extends JpaRepository<Journey, Long> {
+    // JpaRepository provides findAll(), findById(), save(), deleteById(), etc.
+    // We don't need to add anything else here.
 }
